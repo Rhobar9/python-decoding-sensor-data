38fc06a2c439061795e5b6a06890beddfa9b9369
Rhobar9 <86721432+Rhobar9@users.noreply.github.com>
2022-07-24 16:05:46 +0200

    Module5_attempt

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,6 +6,8 @@ from house_info import HouseInfo
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from particle_count_info import ParticleData
+from energy_info import EnergyData
+
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -38,7 +40,7 @@ print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 # Module 4 code here:
 humidity_data = HumidityData(data)
 recs = humidity_data.get_data_by_area(rec_area=test_area)
-print("\nHouse Humidity sensor records for area {} = {}".format((test_area), len(recs)))
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tAverage: {} humidity".format(mean(recs)))
 
 recs = humidity_data.get_data_by_date(rec_date=test_date)
@@ -62,3 +64,16 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 # Module 5 code here
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
